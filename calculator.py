c6dcaacb8bea1b6ce85b56af6738bedf1577d99a
AjCaroline <30448867+AjCaroline@users.noreply.github.com>
2024-12-18 14:10:45 -0500

    Update calculator.py
    INE Lab exercise completion.

--- calculator.py
+++ calculator.py
@@ -3,24 +3,31 @@ def add(x, y):
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
+    return x / y
 
+
+def divide_by_zero(x, y):
+    results = divide(x, y)
+    assert type(results) == str
+    return results
+    
 
 def multiply(x, y):
-    pass
+    return x * y
 
 
 def square(x):
-    pass
+    return square(x, y) 
 
 
 def power(x, y):
-    pass
+    return power(x, y)
 
 
 def sqrt(x):
-    pass
+    return sqrt(x, y)
+    
